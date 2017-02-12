b519548ec294cd13d3c0d348872e87b7f2975193
AnakinStayWorker <freeblue79@gmail.com>
2017-02-12 23:01:11 +0900

    board 04

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var methodOverride = require("method-override");
 var app = express();
 
 // DB setting
-mongoose.connect(process.env.MONGO_DB); // 1
+mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function(){
  console.log("DB connected");
@@ -24,6 +24,7 @@ app.use(methodOverride("_method"));
 
 // Routes
 app.use("/", require("./routes/home"));
+app.use("/posts", require("./routes/posts"));
 
 // Port setting
 app.listen(3000, function(){
